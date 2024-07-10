35466d4cf2afe3735ea4d6f4e655e0f67766a468
Makar Timofeev <makar.timofeev.1990@gmail.com>
2024-07-10 13:58:43 +0300

    add sql comments

--- init/01-init.sql
+++ init/01-init.sql
@@ -1,5 +1,6 @@
 CREATE TABLE script_logs_raw (
     timestamp DateTime64(6),
+    -- Для этого поля я выбрал тип данных DateTime64 с 6 знаками после запятой, так как подумал, что в анализе может пригодиться точностью между звонками
     level String,
     sys String,
     mrf Nullable(String),
@@ -21,6 +22,8 @@ CREATE TABLE script_logs_raw (
     current_script_version Nullable(Float64),
     block_type String,
     parameters Nullable(String),
+    -- Параметры в формате JSON, выбрал строковой тип данных для их хранения
+    -- В параметрах содержатся ключевые поля: ACCOUNT_NUMBER, CALLER_ID, COMMUNICATION_THEME, COMMUNICATION_DETAIL, COMMUNICATION_RESULT
     timezone String,
     communication_id Nullable(Float64),
     communication_number Int64,
@@ -33,9 +36,9 @@ CREATE TABLE script_logs_raw (
     script_responsible Nullable(String),
     current_script_responsible Nullable(String),
     crm_departament Nullable(String)
-) ENGINE = MergeTree()
+) ENGINE = MergeTree() -- MergeTree() с сортировкой по timestamp. Использовал для оптимизации исходя из документации CLickhouse
 ORDER BY timestamp;
 
 INSERT INTO script_logs_raw
-FROM INFILE '/tmp/sm_test_data.csv'
+FROM INFILE '/tmp/sm_test_data.csv' -- Здесь путь указан для докер контейнера
 FORMAT CSV;
